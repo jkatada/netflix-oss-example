62101fb0eb9224172e8070f332c20c69d722af3c
jkatada <jkatada>
2016-11-25 00:15:02 +0900

    Introduce Spring Cloud Netflix

--- src/main/java/com/example/netflix/NetflixOssExampleApplication.java
+++ src/main/java/com/example/netflix/NetflixOssExampleApplication.java
@@ -2,8 +2,10 @@ package com.example.netflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class NetflixOssExampleApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/netflix/feign/FeignController.java
+++ src/main/java/com/example/netflix/feign/FeignController.java
@@ -3,10 +3,13 @@ package com.example.netflix.feign;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -22,11 +25,13 @@ import feign.jackson.JacksonDecoder;
 public class FeignController {
 
 	private final RestTemplate restTemplate;
-	private final UserApi userApi;
+	private final UserClient userClient;
+	@Autowired
+	UserClientSpring userClientSpring;
 
 	public FeignController(RestTemplateBuilder builder) {
-		this.userApi = Feign.builder().decoder(new JacksonDecoder())
-				.target(UserApi.class, "http://localhost:8080");
+		this.userClient = Feign.builder().decoder(new JacksonDecoder())
+				.target(UserClient.class, "http://localhost:8080");
 		this.restTemplate = builder.build();
 
 	}
@@ -34,7 +39,7 @@ public class FeignController {
 	// Feignを使った場合1
 	@GetMapping("/user/{id}")
 	public User user(@PathVariable long id) {
-		User user = userApi.user(id);
+		User user = userClient.user(id);
 		user.setName(user.getName() + " via Feign");
 		return user;
 	}
@@ -42,7 +47,7 @@ public class FeignController {
 	// Feignを使った場合2
 	@GetMapping("/user")
 	public List<User> user() {
-		return userApi.user().stream().map(u -> {
+		return userClient.user().stream().map(u -> {
 			u.setName(u.getName() + " via Feign");
 			return u;
 		}).collect(Collectors.toList());
@@ -57,11 +62,32 @@ public class FeignController {
 		return user;
 	}
 
-	public interface UserApi {
+	// Feign + Spring Cloud Netflix を使った場合
+	@GetMapping("/feignSpring/{id}")
+	public User feignSpringUser(@PathVariable long id) {
+		User user = userClientSpring.user(id);
+		user.setName(user.getName() + " via Feign with Spring Cloud Netflix");
+		return user;
+	}
+
+	public interface UserClient {
 		@RequestLine("GET /feigndummy/user/{id}")
 		User user(@Param("id") long id);
 
 		@RequestLine("GET /feigndummy/user")
 		List<User> user();
 	}
+	
+	// @FeignClientのインターフェースは@Autowiredでインジェクションできる
+	// @FeignClientを付与するとHystrixも有効化
+	@FeignClient(name="userClient", url="http://localhost:8080")
+	public interface UserClientSpring {
+		// Springの@ReqeustMappingが使える。@GetMappingだとエラー
+		@RequestMapping(method = RequestMethod.GET, value = "/feigndummy/user/{id}")
+		User user(@PathVariable("id") long id);
+
+		@RequestMapping(method = RequestMethod.GET, value = "/feigndummy/user")
+		List<User> user();
+	}
+
 }
