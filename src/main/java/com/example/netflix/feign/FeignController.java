afd0271d4003687a99a26469265d9b740952d8ef
jkatada <jkatada>
2016-11-24 23:37:31 +0900

    Polish

--- src/main/java/com/example/netflix/feign/FeignController.java
+++ src/main/java/com/example/netflix/feign/FeignController.java
@@ -1,16 +1,13 @@
 package com.example.netflix.feign;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.netflix.common.User;
