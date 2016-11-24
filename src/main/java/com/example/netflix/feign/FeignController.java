b10895228e583f4f032c152fc1bf2e9b43302804
jkatada <jkatada>
2016-11-24 23:33:30 +0900

    Implement RestTemplate example for comparison

--- src/main/java/com/example/netflix/feign/FeignController.java
+++ src/main/java/com/example/netflix/feign/FeignController.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.netflix.common.User;
 
@@ -20,36 +24,47 @@ import feign.jackson.JacksonDecoder;
 @RequestMapping("/feign")
 public class FeignController {
 
+	private final RestTemplate restTemplate;
+	private final RestUserClient client;
+
+	public FeignController(RestTemplateBuilder builder) {
+		this.client = Feign.builder().decoder(new JacksonDecoder())
+				.target(RestUserClient.class, "http://localhost:8080");
+		this.restTemplate = builder.build();
+
+	}
+
+	// Feignを使った場合1
 	@GetMapping("/user/{id}")
 	public User user(@PathVariable long id) {
-
-		FeignDummy feignDummy = Feign.builder().decoder(new JacksonDecoder())
-				.target(FeignDummy.class, "http://localhost:8080");
-
-		User user = feignDummy.user(id);
+		User user = client.user(id);
 		user.setName(user.getName() + " via Feign");
 		return user;
 	}
 
+	// Feignを使った場合2
 	@GetMapping("/user")
 	public List<User> user() {
-
-		FeignDummy feignDummy = Feign.builder().decoder(new JacksonDecoder())
-				.target(FeignDummy.class, "http://localhost:8080");
-
-		return feignDummy.user().stream().map(u -> {
+		return client.user().stream().map(u -> {
 			u.setName(u.getName() + " via Feign");
 			return u;
 		}).collect(Collectors.toList());
 	}
 
-	public interface FeignDummy {
+	// RestTemplateを使った場合
+	@GetMapping("/restTemplate/{id}")
+	public User restTemplateUser(@PathVariable long id) {
+		User user = restTemplate
+				.getForObject("http://localhost:8080/feigndummy/user/" + id, User.class);
+		user.setName(user.getName() + " via RestTemplate");
+		return user;
+	}
+
+	public interface RestUserClient {
 		@RequestLine("GET /feigndummy/user/{id}")
 		User user(@Param("id") long id);
 
 		@RequestLine("GET /feigndummy/user")
 		List<User> user();
-
 	}
-
 }
