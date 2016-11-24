8687de617176ba68f35099851c2987f260c01855
jkatada <jkatada>
2016-11-24 22:29:09 +0900

    Initial commit

---
+++ src/main/java/com/example/netflix/NetflixOssExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.netflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixOssExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixOssExampleApplication.class, args);
+	}
+}
